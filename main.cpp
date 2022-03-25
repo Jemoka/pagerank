dd1a063754a67e83ff80a6239dde4443d4588123
Houjun Liu <hliu@shabang.cf>
2022-03-25 11:23:39 -0700

    [NW] there is somewhere a segfault

--- main.cpp
+++ main.cpp
@@ -10,6 +10,23 @@
 int main() {
     PageRank pr;
 
+    std::vector<Eigen::Index> a;
+    a.push_back(4);
+    a.push_back(3);
+    a.push_back(2);
+
+    std::vector<Eigen::Index> b;
+    a.push_back(1);
+    a.push_back(3);
+
+    std::vector<Eigen::Index> c;
+    a.push_back(2);
+    a.push_back(1);
+    
+    pr.insert(a);
+    pr.insert(b);
+    pr.insert(c);
+
     return 0;
 }
 
